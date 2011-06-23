dc3ec0679693eeb38055c60c667fe0821228ac26
Joerg Roedel <joerg.roedel@amd.com>
2011-06-23 17:07:52 +0200

    KVM: IOMMU: Use iommu_commit() in device-passthrough code
    Use the new iommu_commit() function in the KVM device
    passthrough code.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- virt/kvm/iommu.c
+++ virt/kvm/iommu.c
@@ -187,6 +187,8 @@ int kvm_assign_device(struct kvm *kvm,
 		PCI_SLOT(assigned_dev->host_devfn),
 		PCI_FUNC(assigned_dev->host_devfn));
 
+	iommu_commit(domain);
+
 	return 0;
 out_unmap:
 	kvm_iommu_unmap_memslots(kvm);
@@ -215,6 +217,8 @@ int kvm_deassign_device(struct kvm *kvm,
 		PCI_SLOT(assigned_dev->host_devfn),
 		PCI_FUNC(assigned_dev->host_devfn));
 
+	iommu_commit(domain);
+
 	return 0;
 }
 
@@ -234,6 +238,8 @@ int kvm_iommu_map_guest(struct kvm *kvm)
 	r = kvm_iommu_map_memslots(kvm);
 	if (r)
 		goto out_unmap;
+
+	iommu_commit(kvm->arch.iommu_domain);
 
 	return 0;
 
@@ -283,6 +289,8 @@ static void kvm_iommu_put_pages(struct kvm *kvm,
 
 		gfn += unmap_pages;
 	}
+
+	iommu_commit(domain);
 }
 
 static int kvm_iommu_unmap_memslots(struct kvm *kvm)
